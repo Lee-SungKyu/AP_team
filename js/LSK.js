ef2ce86d2880d7f2abe447392a689f5f00330e0d
이성규 <22000520@handong.edu>
2021-07-08 20:54:35 +0900

    .

--- js/LSK.js
+++ js/LSK.js
@@ -1,4 +1,16 @@
+// 03 section, alert quote
 function alert1(text)
 {
     alert(text);
 }
+
+// 02 section, swap text
+function myFunction() {
+    var x = document.getElementById("myDIV");
+    
+    if (x.innerHTML === "Hello") {
+      x.innerHTML = "Swapped text!";
+    } else {
+      x.innerHTML = "Hello";
+    }
+  }
